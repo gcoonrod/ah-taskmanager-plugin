98fea8ede01952ea4ef4fc81b1f55200e0bed939
Greg Coonrod <coogr02@ca.com>
2015-05-02 15:51:27 -0500

    more post install updates

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -6,7 +6,26 @@ var path = require('path');
 var localFile   = path.normalize(__dirname + '/../config/ah-taskmanager-plugin.js');
 var projectFile = path.normalize(process.cwd() + '/../../config/plugins/ah-taskmanager-plugin.js');
 
-if(!fs.existsSync(projectFile)){
-    console.log("copying " + localFile + " to " + projectFile);
-    fs.createReadStream(localFile).pipe(fs.createWriteStream(projectFile));
+function copyFile(source, target) {
+    'use strict';
+    var rd = fs.createReadStream(source);
+    rd.on("error", function(err) {
+        console.error(err);
+    });
+    var wr = fs.createWriteStream(target);
+    wr.on("error", function(err) {
+        console.error(err);
+    });
+    wr.on("close", function(ex) {
+        console.log("Done copying file.")
+    });
+    rd.pipe(wr);
 }
+
+if(!fs.existsSync(projectFile)){
+    console.log("Copying configuration files for TaskManager Plugin");
+    console.log("copying " + localFile + " to " + projectFile);
+    copyFile(localFile, projectFile);
+
+}
+
