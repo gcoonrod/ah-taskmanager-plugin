f5aa433d2a194cb3cbef78ec483933c0335a0dcc
Greg Coonrod <coogr02@ca.com>
2015-05-02 14:18:18 -0500

    more initializers stubs

--- initializers/mongoose.js
+++ initializers/mongoose.js
@@ -4,11 +4,11 @@ module.exports = {
     startPriority: 500,
     loadPriority: 500,
     initialize: function(api, next) {
-        api.log("Initializing TaskManger Plugin.", "debug");
+        api.log("Initializing Mongoose Initializer.", "debug");
+
         next();
     },
     start: function(api, next) {
-        api.log("Starting TaskManager Plugin.", "debug");
-        next();
+        api.log("Starting Mongoose Initializer.", "debug");
     }
 };
