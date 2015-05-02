2c05c43fecfecb17598ea5efb5b445a69a554c05
Greg Coonrod <coogr02@ca.com>
2015-05-02 16:31:29 -0500

    modernizing postinstall.js

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,32 +1,32 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
+var fs = require('fs-extra');
 var path = require('path');
 
 var localFile   = path.normalize(__dirname + '/../config/ah-taskmanager-plugin.js');
-var projectFile = path.normalize(process.cwd() + '/../../config/plugins/ah-taskmanager-plugin.js');
+var projectFile = path.normalize('/ah-taskmanager-plugin.js');
+var pluginFolder = path.normalize(process.cwd() + '/../../config/plugins');
 
-function copyFile(source, target) {
-    'use strict';
-    var rd = fs.createReadStream(source);
-    rd.on("error", function(err) {
-        console.error(err);
-    });
-    var wr = fs.createWriteStream(target);
-    wr.on("error", function(err) {
-        console.error(err);
-    });
-    wr.on("close", function(ex) {
-        console.log("Done copying file.")
-    });
-    rd.pipe(wr);
-}
-
-if(!fs.existsSync(projectFile)){
-    console.log("Copying configuration files for TaskManager Plugin");
-    console.log("copying " + localFile + " to " + projectFile);
-    copyFile(localFile, projectFile);
-} else {
-    console.log("Config file already exists!");
-}
+console.log("Copying configuration files for TaskManager ActionHero plugin");
+fs.ensureDir(pluginFolder, function(err){
+    if(err){
+        console.error("Unable to ensure that '" + pluginFolder + "' exists!", err);
+    } else {
+        fs.stat(pluginFolder.concat(projectFile), function(err, stats){
+            if(err == null) {
+                console.log('Config file already exists!');
+            } else if(err.code == 'ENOENT') {
+                fs.copy(localFile, pluginFolder.concat(projectFile), function(err){
+                    if(err){
+                        console.error("Unable to copy configuration files!", err);
+                    } else {
+                        console.log("Successfully copied TaskManager configuration files!");
+                    }
+                });
+            } else {
+                console.log('Some other error: ', err.code);
+            }
+        });
+    }
+});
 
